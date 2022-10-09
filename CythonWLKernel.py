54ce265ec36f992d06e73c07c38a2e14af920a4c
feather16 <hadarion721@gmail.com>
2022-10-09 18:57:20 +0900

    不要なコメントの削除

--- CythonWLKernel.py
+++ CythonWLKernel.py
@@ -13,9 +13,7 @@ class NATSBenchWLKernelFromCounters(Protocol):
 class NATSBenchWLKernel(Protocol):
     def __call__(self, cell1: list[int], cell2: list[int], H: int = 2) -> float:
         ...
-#class NATSBenchWLKernelVector(Protocol):
-#    def __call__(self, cell1: list[int], cells: list[list[int]], H: int = 2) -> list[float]:
-        ...
+
 natsbench_cell_to_wl_vector: NATSBenchCellToWLVector = \
     import_module('cython_wl_kernel').cython_natsbench_cell_to_wl_kernel_vector
 natsbench_cell_to_wl_counter: NATSBenchCellToWLCounter = \
@@ -24,5 +22,3 @@ natsbench_wl_kernel_from_wl_counters: NATSBenchWLKernelFromCounters = \
     import_module('cython_wl_kernel').cython_natsbench_wl_kernel_from_wl_counters
 natsbench_wl_kernel: NATSBenchWLKernel = \
     import_module('cython_wl_kernel').cython_natsbench_wl_kernel
-#natsbench_wl_kernel_vector: NATSBenchWLKernelVector = \
-#    import_module('cython_wl_kernel').cython_natsbench_wl_kernel_vector
